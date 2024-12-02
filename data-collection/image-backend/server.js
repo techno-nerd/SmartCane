53861cbe3a824b2ca17da00cbb3121632bee5743
Arush Garg <arushg0312@gmail.com>
2024-12-01 17:14:00 -0800

    Crop image to square

--- data-collection/image-backend/server.js
+++ data-collection/image-backend/server.js
@@ -2,12 +2,13 @@ const express = require('express');
 const multer = require('multer');
 const fs = require('fs');
 const path = require('path');
+const sharp = require('sharp');
 
 
 const TEMP_DIR = 'saved_images/temp'
 const HAZARD_DIR = 'saved_images/hazard'
 const NON_HAZARD_DIR = 'saved_images/non-hazard'
-
+const IMAGE_WIDTH = 512
 
 const app = express();
 const port = 3000;
@@ -16,7 +17,7 @@ const port = 3000;
 const upload = multer({ dest: TEMP_DIR }); // Temporary folder for uploads
 
 // POST route to handle file upload
-app.post('/upload', upload.single('image'), (req, res) => {
+app.post('/upload', upload.single('image'), async (req, res) => {
     try {
         console.log('Received body:', req.body); // Log form fields
         console.log('Received file:', req.file); // Log uploaded file details
@@ -37,15 +38,19 @@ app.post('/upload', upload.single('image'), (req, res) => {
         const oldPath = req.file.path;
         const newPath = path.join(targetDir, req.file.originalname);
 
-        fs.rename(oldPath, newPath, (err) => {
-            if (err) {
-                console.error('Error moving file:', err);
-                return res.status(500).send('Error saving file');
-            }
+        await sharp(req.file.path)
+            .resize({
+                width: IMAGE_WIDTH,
+                height: IMAGE_WIDTH,
+                fit: sharp.fit.cover,
+                position: sharp.strategy.attention, // Use "attention" to crop around the most relevant part of the image
+            })
+            .toFile(newPath);
+        
+        console.log(`Cropped image saved to ${newPath}`);
+        fs.unlinkSync(req.file.path);
+        res.send({ message: 'File uploaded and cropped successfully', filePath: newPath });
 
-            console.log(`File saved to ${newPath}`);
-            res.send('File uploaded successfully');
-        });
     } catch (error) {
         console.error('Error processing upload:', error);
         res.status(500).send('Server error');
