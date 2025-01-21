3e5db3600ee5dfb7423a3de067c5314214614b2c
Arush Garg <arushg0312@gmail.com>
2025-01-20 18:39:01 -0800

    Added stream delay

--- main/GenAICane/components/CameraInput.tsx
+++ main/GenAICane/components/CameraInput.tsx
@@ -5,31 +5,39 @@ import HandleClick from './HandleClick';
 import * as FileSystem from 'expo-file-system';
 import { getDescription, getPrediction } from './GetPrediction';
 import * as Speech from 'expo-speech';
+import deleteFile from './DeleteFile';
 
 
 /**Time in milliseconds between POST request to server for hazard detection */
-const STREAM_PERIOD = 2000;
+const STREAM_PERIOD = 10000;
+
+/**Cooldown in milliseconds between automated hazard description */
+const DESCRIBE_COOLDOWN = 60000;
 
 export default function CameraInput() {
   const [facing, setFacing] = useState<CameraType>('back');
   const [permission, requestPermission] = useCameraPermissions();
   const ref = useRef<CameraView>(null);
+  let timeDescribed = 0;
   
   useEffect(() => {
     if (permission && permission.granted) {
       const interval = setInterval(async () => {
         if (ref.current) {
-          const photo = await ref.current.takePictureAsync();
+          const photo = await ref.current.takePictureAsync({ shutterSound: false });
           if(photo) {
+            let timeStart = Date.now();
             const base64 = await FileSystem.readAsStringAsync(photo.uri, { encoding: FileSystem.EncodingType.Base64 });
             const hazard = await getPrediction(base64);
             console.log(hazard);
-            if(hazard == "1") {
+            if(hazard == "1" && Date.now()-timeDescribed >= DESCRIBE_COOLDOWN) {
               Speech.speak("Hazard detected! Generating description now");
               const description = await getDescription(base64);
               Speech.speak(description.description);
+              timeDescribed = Date.now();
             }
-
+            console.log(Date.now()-timeStart);
+            deleteFile(photo.uri);
           }
         }
       }, STREAM_PERIOD);
@@ -72,21 +80,5 @@ const styles = StyleSheet.create({
   },
   camera: {
     flex: 1,
-  },
-  buttonContainer: {
-    flex: 1,
-    flexDirection: 'row',
-    backgroundColor: 'transparent',
-    margin: 64,
-  },
-  button: {
-    flex: 1,
-    alignSelf: 'flex-end',
-    alignItems: 'center',
-  },
-  text: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    color: 'white',
   },
 });
